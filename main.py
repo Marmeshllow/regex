c32300a55d91eb43b1abdaf254dae1555d977a77
Arzamastsev Dmitry <motok1996@gmail.com>
2021-11-19 01:35:33 +0300

    ggwp

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import re
-from pprint import pprint
 import csv
-r = r'(\+7|8)?\s?\(?(\d{3})\)?\s*-?(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})(\s?\(?(доб\.)\s?(\d{4})\)?)?'
-sub = r'+7(\2)-\3-\4-\5 \7\8'
+
+pattern = re.compile(r"(\+7|8)?\s?\(?(\d{3})\)?\s*-?(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})(\s?\(?(доб\.)\s?(\d{4})\)?)?")
+sub = r"+7(\2)\3-\4-\5 \7\8"
 
 with open("data.csv", encoding="utf-8") as f:
     rows = csv.reader(f, delimiter=",")
@@ -17,31 +17,32 @@ for el in contacts_list:
         sent_list.remove("")
     el[0], el[1], el[2] = sent_list[0], sent_list[1], sent_list[2]
 
-
-for num, el in enumerate(contacts_list):
+prepare_to_del = []
+for num1, el in enumerate(contacts_list):
     double = []
-    for elem in contacts_list[num + 1:]:
-        if el[0] + el[1] == elem[0] + elem[1]:
-            double.append(elem)
-    if len(double) > 0:
-        pprint(double)
+    if num1 not in prepare_to_del:
+        num2 = num1 + 1
+        for elem in contacts_list[num2:]:
+            if el[0] + el[1] == elem[0] + elem[1]:
+                double.append(elem)
+                num2 += 1
+                prepare_to_del.append(num2)
+        if len(double) > 0:
+            for row in double:
+                for num3, call in enumerate(row):
+                    if len(el[num3]) < len(call):
+                        el[num3] = call
 
+for i in prepare_to_del[::-1]:
+    del contacts_list[i]
+print(contacts_list)
+print(prepare_to_del)
 
+for el in contacts_list:
+    result = pattern.sub(sub, el[5])
+    el[5] = result
+print(contacts_list)
 
-
-
-
-s = '''
-делаем энемерате !!!
-бахаем вайл от нашего индекса и до конца!!!
-если находим мач то апендим в промеж список и удаляем из первичного списка!!!
-сравниваем значения из нашей похиции со значениями из промеж списка по длинее??? по нул? по ''? !!!
-оставляем то что пизже'''
-
-
-
-# код для записи файла в формате CSV
-# with open("phonebook.csv", "w") as f:
-#   datawriter = csv.writer(f, delimiter=',')
-#   # Вместо contacts_list подставьте свой список
-#   datawriter.writerows(contacts_list)
+with open("phonebook.csv", "w", newline='') as f:
+    dw = csv.writer(f, delimiter=',')
+    dw.writerows(contacts_list)
