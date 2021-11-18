61e2fa91e3606113d9eac0f79372c3d1b5349f3f
Arzamastsev Dmitry <motok1996@gmail.com>
2021-11-19 02:18:56 +0300

    classes

--- main.py
+++ main.py
@@ -1,48 +1,63 @@
 import re
 import csv
 
-pattern = re.compile(r"(\+7|8)?\s?\(?(\d{3})\)?\s*-?(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})(\s?\(?(доб\.)\s?(\d{4})\)?)?")
-sub = r"+7(\2)\3-\4-\5 \7\8"
 
-with open("data.csv", encoding="utf-8") as f:
-    rows = csv.reader(f, delimiter=",")
-    contacts_list = list(rows)
+class Regex:
+    def __init__(self):
+        self.contacts_list = []
 
-del contacts_list[0]
+    def open_data(self, filename):
+        with open(filename, encoding="utf-8") as f:
+            rows = csv.reader(f, delimiter=",")
+            self.contacts_list = list(rows)
+        del self.contacts_list[0]
 
-for el in contacts_list:
-    full_name = el[0] + ' ' + el[1] + ' ' + el[2]
-    sent_list = re.split(r'\s+', full_name)
-    if "" in sent_list and len(sent_list) > 3:
-        sent_list.remove("")
-    el[0], el[1], el[2] = sent_list[0], sent_list[1], sent_list[2]
+    def reg_fio(self):
+        for el in self.contacts_list:
+            full_name = el[0] + ' ' + el[1] + ' ' + el[2]
+            sent_list = re.split(r'\s+', full_name)
+            if "" in sent_list and len(sent_list) > 3:
+                sent_list.remove("")
+            el[0], el[1], el[2] = sent_list[0], sent_list[1], sent_list[2]
 
-prepare_to_del = []
-for num1, el in enumerate(contacts_list):
-    double = []
-    if num1 not in prepare_to_del:
-        num2 = num1 + 1
-        for elem in contacts_list[num2:]:
-            if el[0] + el[1] == elem[0] + elem[1]:
-                double.append(elem)
-                num2 += 1
-                prepare_to_del.append(num2)
-        if len(double) > 0:
-            for row in double:
-                for num3, call in enumerate(row):
-                    if len(el[num3]) < len(call):
-                        el[num3] = call
+    def dell_double(self):
+        prepare_to_del = []
+        for num1, el in enumerate(self.contacts_list):
+            double = []
+            if num1 not in prepare_to_del:
+                num2 = num1 + 1
+                for elem in self.contacts_list[num2:]:
+                    if el[0] + el[1] == elem[0] + elem[1]:
+                        double.append(elem)
+                        num2 += 1
+                        prepare_to_del.append(num2)
+                        if len(double) > 0:
+                            for row in double:
+                                for num3, call in enumerate(row):
+                                    if len(el[num3]) < len(call):
+                                        el[num3] = call
+        for i in prepare_to_del[::-1]:
+            del self.contacts_list[i]
 
-for i in prepare_to_del[::-1]:
-    del contacts_list[i]
-print(contacts_list)
-print(prepare_to_del)
+    def reg_phone(self, pattern, sub):
+        for el in self.contacts_list:
+            result = pattern.sub(sub, el[5])
+            el[5] = result
+        print(self.contacts_list)
 
-for el in contacts_list:
-    result = pattern.sub(sub, el[5])
-    el[5] = result
-print(contacts_list)
+    def save_data(self, final_filename):
+        with open(final_filename, "w", newline='') as f:
+            dw = csv.writer(f, delimiter=',')
+            dw.writerows(self.contacts_list)
 
-with open("phonebook.csv", "w", newline='') as f:
-    dw = csv.writer(f, delimiter=',')
-    dw.writerows(contacts_list)
+
+if __name__ == '__main__':
+    pattern1 = re.compile(r"(\+7|8)?\s?\(?(\d{3})\)?\s*-?(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})(\s?\(?(доб\.)\s?(\d{4})\)?)?")
+    sub1 = r"+7(\2)\3-\4-\5 \7\8"
+
+    a = Regex()
+    a.open_data('data.csv')
+    a.reg_fio()
+    a.dell_double()
+    a.reg_phone(pattern1, sub1)
+    a.save_data('phonebook.csv')
